f2ba501b60f3cbf5cad3565eaab0cba8bbf8c65e
Shubham <shubhamkumarwinner@gmail.com>
2021-09-26 22:04:12 +0530

    implementing tab layout

--- app/src/main/java/com/shubhamkumarwinner/composeviewpagertablayout/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composeviewpagertablayout/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -22,6 +18,7 @@ import com.google.accompanist.pager.*
 import com.google.android.material.animation.*
 import com.google.android.material.math.MathUtils.lerp
 import com.shubhamkumarwinner.composeviewpagertablayout.ui.theme.ComposeViewPagerTabLayoutTheme
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
 @ExperimentalPagerApi
@@ -48,57 +45,65 @@ fun HorizontalViewPager() {
         R.drawable.pic2,
         R.drawable.pic3,
         R.drawable.pic4,
-        R.drawable.pic5,
-        R.drawable.pic6,
-        R.drawable.pic7,
-        R.drawable.pic8,
-        R.drawable.pic9,
-        R.drawable.pic10,
-        R.drawable.pic11
     )
     val pagerState = rememberPagerState(pageCount = listOfImage.size, initialOffscreenLimit = 2)
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column {
-            HorizontalPager(state = pagerState) { page ->
-                Card(
-                    Modifier
-                        .graphicsLayer {
-                            val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
-                            lerp(
-                                0.65f,
-                                1f,
-                                1f - pageOffset.coerceIn(0f, 1f)
-                            ).also { scale ->
-                                scaleX = scale
-                                scaleY = scale
-                            }
-
-                            alpha = lerp(
-                                0.5f,
-                                1f,
-                                1f - pageOffset.coerceIn(0f, 1f)
-                            )
-                        }
-                ) {
-                    Image(
-                        painter = painterResource(id = listOfImage[page]),
-                        contentDescription = "image$page",
-                        contentScale = ContentScale.FillWidth,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
+    val scope = rememberCoroutineScope()
+    Column (modifier = Modifier.fillMaxSize()){
+        TabRow(
+            // Our selected tab is our current page
+            selectedTabIndex = pagerState.currentPage,
+            // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
             }
-            HorizontalPagerIndicator(
-                pagerState = pagerState,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(16.dp),
-            )
+        ) {
+            // Add tabs for all of our pages
+            listOfImage.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text("Pic $index", color = Color.White) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
         }
+
+        HorizontalPager(state = pagerState) { page ->
+            Card(
+                Modifier.fillMaxSize()
+                    .graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                        lerp(
+                            0.65f,
+                            1f,
+                            1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+
+                        alpha = lerp(
+                            0.5f,
+                            1f,
+                            1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+            ) {
+                Image(
+                    painter = painterResource(id = listOfImage[page]),
+                    contentDescription = "image$page",
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
     }
 
 }
