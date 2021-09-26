b2cc4c0e064c6eb8b77bff32959b6214346de7d8
Shubham <shubhamkumarwinner@gmail.com>
2021-09-26 20:53:58 +0530

    horizontal pager

--- app/src/main/java/com/shubhamkumarwinner/composeviewpagertablayout/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composeviewpagertablayout/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.shubhamkumarwinner.composeviewpagertablayout
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.shubhamkumarwinner.composeviewpagertablayout.ui.theme.ComposeViewPagerTabLayoutTheme
 
+@ExperimentalPagerApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +27,46 @@ class MainActivity : ComponentActivity() {
             ComposeViewPagerTabLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    HorizontalViewPager()
                 }
             }
         }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun HorizontalViewPager() {
+    val pagerState = rememberPagerState(pageCount = 12, initialOffscreenLimit = 2)
+
+    HorizontalPager(state = pagerState) { page ->
+        val listOfImage = listOf(
+            R.drawable.pic0,
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8,
+            R.drawable.pic9,
+            R.drawable.pic10,
+            R.drawable.pic11
+        )
+        // Our page content
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = listOfImage[page]),
+                contentDescription = "image$page",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
     }
 }
 
